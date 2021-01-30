ffb119697cef4cef3ec621ddf85f650960f3c3d3
KathleenEdie <kathleenedie@gmail.com>
2021-01-30 15:20:58 +0000

    Hello World, flask operating

--- bucket_list/app.py
+++ bucket_list/app.py
@@ -1,2 +1,9 @@
+from flask import Flask
+    #import flask and render template
+
+app = Flask(__name__)
+
+from controllers import cities_controller
+
 if __name__=='__main__':
     app.run(debug=True)
